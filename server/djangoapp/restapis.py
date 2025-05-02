a52a425fd95369fda78ffe59b989d85b41602da9
Marouene Mrabet <mrabet.marouene@gmail.com>
2025-05-02 18:17:18 -0400

    updates

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -47,7 +47,6 @@ def post_review(data_dict):
     request_url = backend_url + "/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
-        print(response.json())
         return response.json()
     except Exception as e:
         print("Network exception" + e)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -92,9 +92,9 @@ def get_dealer_reviews(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
-        for review_detail in reviews:
-            response = analyze_review_sentiments(review_detail['review'])
-            review_detail['sentiment'] = response['sentiment']
+        #for review_detail in reviews:
+        #    response = analyze_review_sentiments(review_detail['review'])
+        #    review_detail['sentiment'] = response['sentiment']
         return JsonResponse({"status":200,"reviews":reviews})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
@@ -123,7 +123,6 @@ def add_review(request):
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
-    print(count)
     if(count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
